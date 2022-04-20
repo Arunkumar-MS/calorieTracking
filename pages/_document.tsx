b859e9028dff5ec65e4e1938b26d4f6736e3116e
Arunkumar <arunkumarms3287@gmail.com>
2022-04-20 08:43:05 +0000

    party

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,7 +9,7 @@ class MyDocument extends Document {
   render() {
     return (
       <Html className='h-full bg-gray-100' lang="en-us">
-          <script type="text/javascript" src="http://localhost:3000/~partytown/partytown.js"></script>
+          <script type="text/javascript" src="./~partytown/partytown.js"></script>
           <script type="text/partytown" src="//tags.tiqcdn.com/utag/agoda/main2018/prod/utag.js"></script>
         <title>
           Caloriese tracker
